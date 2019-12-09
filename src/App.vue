f5e04543fecfa31f777914c077e737eb290da6dd
Artem <temorfeouz@gmail.com>
2019-12-10 00:52:34 +0200

    !!!

--- src/App.vue
+++ src/App.vue
@@ -61,73 +61,54 @@ import { axios } from '@/plugins/axios'
           // this.$data.servdata=response.data
 
 
-try {
-  response.data.split(`
-`).forEach(v=>{
-    console.log( JSON.parse(v))
-  })
-
-}catch (e) {
-  console.log(e)
-}
-
           this.$data.CombineOptions= {
             'chart': {
               'title': 'Combination of Column, Spline-Area and Spline Chart',
               'animation': true,
               'tooltip': {'displayMode': 'union'},
               'interactivity': {'hoverMode': 'by-x'},
-              'series': [{
-                'seriesType': 'column',
-                'name': 'Column',
-                'data': [
-                  {'x': 'P1', 'value': 5854},
-                  {'x': 'P2', 'value': 4171},
-                  {'x': 'P3', 'value': 1375},
-                  {'x': 'P4', 'value': 1875},
-                  {'x': 'P5', 'value': 2246},
-                  {'x': 'P6', 'value': 2696},
-                  {'x': 'P7', 'value': 1287},
-                  {'x': 'P8', 'value': 2140},
-                  {'x': 'P9', 'value': 1603},
-                  {'x': 'P10', 'value': 1628}
-                ]
-              }, {
-                'seriesType': 'spline-area',
-                'name': 'Spline',
-                'legendItem': {'enabled': true, 'iconType': 'square', 'iconStroke': 'none'},
-                'data': [
-                  {'x': 'P1', 'value': 3242},
-                  {'x': 'P2', 'value': 3171},
-                  {'x': 'P3', 'value': 700},
-                  {'x': 'P4', 'value': 1287},
-                  {'x': 'P5', 'value': 1856},
-                  {'x': 'P6', 'value': 1126},
-                  {'x': 'P7', 'value': 987},
-                  {'x': 'P8', 'value': 1610},
-                  {'x': 'P9', 'value': 903},
-                  {'x': 'P10', 'value': 928}
-                ]
-              }, {
+              'series': [ {
                 'seriesType': 'spline',
                 'name': 'Line',
                 'normal': {'stroke': {'color': '#ef6c00', 'thickness': 2.5}},
                 'data': [
-                  {'x': 'P1', 'value': 1740},
-                  {'x': 'P2', 'value': 1970},
-                  {'x': 'P3', 'value': 1550},
-                  {'x': 'P4', 'value': 1500},
-                  {'x': 'P5', 'value': 6600},
-                  {'x': 'P6', 'value': 8500},
-                  {'x': 'P7', 'value': 3700},
-                  {'x': 'P8', 'value': 1000},
-                  {'x': 'P9', 'value': 4400},
-                  {'x': 'P10', 'value': 3220}
+                  // {'x': 'P1', 'value': 1740},
+                  // {'x': 'P2', 'value': 1970},
+                  // {'x': 'P3', 'value': 1550},
+                  // {'x': 'P4', 'value': 1500},
+                  // {'x': 'P5', 'value': 6600},
+                  // {'x': 'P6', 'value': 8500},
+                  // {'x': 'P7', 'value': 3700},
+                  // {'x': 'P8', 'value': 1000},
+                  // {'x': 'P9', 'value': 4400},
+                  // {'x': 'P10', 'value': 3220}
                 ]
               }],
               'type': 'column'
             }
           }
+
+
+          try {
+            response.data.split(`
+`).forEach(v=>{
+ var js =JSON.parse(v)
+
+  if (js.metric!=undefined && js.metric.__name__=="measurement_humidity"){
+    this.$data.CombineOptions.chart.series.data=[];
+   for ( var i=0;i<js.values.length;i++){
+     // console.log(this.$data.CombineOptions.chart.series)
+     this.$data.CombineOptions.chart.series.data.push({'x': js.timestamps[i], 'value': js.values[i]})
+   }
+
+    console.log(this.$data.CombineOptions.chart.series.data)
+  }
+            })
+
+          }catch (e) {
+            console.log(e)
+          }
+
         })
         .catch((error) => console.log(error.response.data));
 
